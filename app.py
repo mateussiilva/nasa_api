a6adb0fe6e7f325fa6374aec2e112fac0e269a6a
mateussiilva <mateuspython3010@gmail.com>
2023-12-28 22:27:19 -0300

    Mudando o comportamento do backend

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask,render_template,request
-
+from PyJson import PyJson
 
 app = Flask(__name__)
 
 
+
+
 @app.route("/")
 def home_page():
-    return render_template("home.html",title_page="Pagina Inicial")
+    return render_template(
+        "home.html",title_page="Pagina Inicial",
+        dados_imagem = PyJson('dados.json').ler_json()
+        )
 
 
 
--- main.py
+++ main.py
@@ -1,25 +1,37 @@
 import requests
-import wget
-
+import urllib.request
+from json import dump
+from os.path import join
 from pprint import pprint as print
 
+def download_image(url_file,name_file):
+	try:
+		urllib.request.urlretrieve(url_file,name_file)
+	except Exception as e:
+		print(e)
+	print("Download sucess")
 
-def baixar_imagem(url_image:str,path_image:str):
-    wget.download(url_image,path_image)
-def criar_pasta():
-    pass
-
-
+# CONSTANTES
+PATH = "/home/mateus/projetos_pessoais/nasa_api/assets/images"
 KEY_API = "p26GqFEtUnVjtAy0yRx9EtyX9asPRFhgUjaKarzd"
+
 url = f"https://api.nasa.gov/planetary/apod?api_key={KEY_API}"
 response = requests.get(url)
-response_json = response.json()
-# chaves = tuple(map(lambda k: k,response_json))
-# print(chaves)
-titulo = response_json.get("title")
-url_image = response_json.get("url")
-path_image = titulo + ".jpg"
+dados = dict()
 
 if response.status_code == 200:
-    baixar_imagem(url_image,path_image)
-    print("Imagem Baixada com sucesso!")
+	json_response = response.json()
+	imagem = json_response.get("url")
+	nome_image = json_response.get("title")+".jpg"
+
+	download_image(imagem,join(PATH,nome_image))
+ 
+	dados["image"] = join(PATH,nome_image)
+	dados["titulo"] = json_response.get("title")
+	dados["texto"] = json_response.get("explanation")
+	dados["autor"] = json_response.get("copyright")
+	with open("dados.json","w+") as file:
+		dump(dados,file)
+
+else:
+	print("Erro, verifique a url")
